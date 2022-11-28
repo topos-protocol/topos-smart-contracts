ab3178c52ce403bd5cb6e0941d4293ec6348054b
Jawad Tariq <sjcool420@hotmail.co.uk>
2022-11-28 15:40:19 -0500

    feat: change peer Id to user defined type
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

--- brownie/contracts/topos-core/TCENodeRegistrator.sol
+++ brownie/contracts/topos-core/TCENodeRegistrator.sol
@@ -1,24 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+type PeerId is bytes32; // type of peer IDs
+
 contract TCENodeRegistrator {
-    error TCENodeAlreadyRegistered(bytes peerId);
+    error TCENodeAlreadyRegistered(PeerId peerId);
 
     struct TCENode {
-        bytes peerId;
+        PeerId peerId;
         bool isPresent;
     }
 
     /// @notice Mapping to store the registered TCE nodes
     /// @dev PeerId => TCENode
-    mapping(bytes => TCENode) tceNodes;
+    mapping(PeerId => TCENode) tceNodes;
 
     /// @notice New TCE node registration event
-    event NewTCENodeRegistered(bytes peerId);
+    event NewTCENodeRegistered(PeerId peerId);
 
     /// @notice Register a new TCE node
     /// @param peerId peer ID of the TCE node
-    function registerTCENode(bytes calldata peerId) public {
+    function registerTCENode(PeerId peerId) public {
         if (tceNodes[peerId].isPresent) revert TCENodeAlreadyRegistered(peerId);
         TCENode memory tceNode = TCENode(peerId, true);
         tceNodes[peerId] = tceNode;
