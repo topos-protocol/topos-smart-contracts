52e9f8415bbbf1026c16c567964857ae0409ed54
Jawad Tariq <sjcool420@hotmail.co.uk>
2022-11-29 16:51:03 -0500

    chore: adapt to improve readability changes
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

--- brownie/tests/unit/test_subnet_registrator.py
+++ brownie/tests/unit/test_subnet_registrator.py
@@ -1,47 +1,47 @@
 import brownie
 
-endpoint = brownie.convert.to_bytes("0xdead", "bytes")
-logo_url = brownie.convert.to_bytes("0xdeed", "bytes")
-name = "Test Subnet"
-public_key = brownie.convert.to_bytes("0xdeaf", "bytes")
+import const as c
 
 
 def test_register_subnet_reverts_on_already_registered(
     alice, subnet_registrator
 ):
-    subnet_registrator.registerSubnet(
-        endpoint, logo_url, name, public_key, {"from": alice}
-    )
+    register_subnet(alice, subnet_registrator)
     # should revert since subnet is already registered
     with brownie.reverts():
-        subnet_registrator.registerSubnet(
-            endpoint, logo_url, name, public_key, {"from": alice}
-        )
+        register_subnet(alice, subnet_registrator)
 
 
 def test_register_subnet_emits_event(alice, subnet_registrator):
-    tx = subnet_registrator.registerSubnet(
-        endpoint, logo_url, name, public_key, {"from": alice}
-    )
+    tx = register_subnet(alice, subnet_registrator)
     assert tx.events["NewSubnetRegistered"].values() == [
-        brownie.convert.datatypes.HexString(public_key, "bytes")
+        brownie.convert.datatypes.HexString(c.SUBNET_PUBLIC_KEY, "bytes")
     ]
 
 
 def test_remove_subnet_reverts_on_not_registered(alice, subnet_registrator):
     # should revert since subnet is not registered
     with brownie.reverts():
-        subnet_registrator.removeSubnet(public_key, {"from": alice})
+        subnet_registrator.removeSubnet(c.SUBNET_PUBLIC_KEY, {"from": alice})
 
 
 def test_remove_subnet_emits_event(alice, subnet_registrator):
-    subnet_registrator.registerSubnet(
-        endpoint, logo_url, name, public_key, {"from": alice}
-    )
-    tx = subnet_registrator.removeSubnet(public_key, {"from": alice})
+    register_subnet(alice, subnet_registrator)
+    tx = subnet_registrator.removeSubnet(c.SUBNET_PUBLIC_KEY, {"from": alice})
     expected = False
-    subnet = subnet_registrator.subnets(public_key)
+    subnet = subnet_registrator.subnets(c.SUBNET_PUBLIC_KEY)
     assert subnet["isPresent"] == expected
     assert tx.events["SubnetRemoved"].values() == [
-        brownie.convert.datatypes.HexString(public_key, "bytes")
+        brownie.convert.datatypes.HexString(c.SUBNET_PUBLIC_KEY, "bytes")
     ]
+
+
+# internal functions #
+def register_subnet(alice, subnet_registrator):
+    return subnet_registrator.registerSubnet(
+        c.ENDPOINT,
+        c.LOGO_URL,
+        c.SUBNET_NAME,
+        c.SUBNET_PUBLIC_KEY,
+        {"from": alice},
+    )
