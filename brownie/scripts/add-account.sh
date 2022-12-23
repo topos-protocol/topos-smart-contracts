42d841b5749017b5c011bb2fb4c5571ff494d56b
sebastiendan <sebastien.dan@gmail.com>
2022-12-22 19:15:32 -0500

    feat: add script to add account

---
+++ brownie/scripts/add-account.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/expect -f
+
+set id [lindex $argv 0];
+set privateKey [lindex $argv 1];
+set password [lindex $argv 2];
+
+spawn brownie accounts new $id
+expect "Enter the private key you wish to add:" 
+send "$privateKey\n"
+expect "Enter the password to encrypt this account with:" 
+send "[lindex $argv 2]\n"
+expect "$ "
