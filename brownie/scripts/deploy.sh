d57ce6682eb35753461ac4367d80c6b24764e975
sebastiendan <sebastien.dan@gmail.com>
2022-12-23 19:21:22 -0500

    feat: handle any number of arguments on contract deployment

--- brownie/scripts/deploy.sh
+++ brownie/scripts/deploy.sh
@@ -4,8 +4,9 @@ set contract [lindex $argv 0];
 set account [lindex $argv 1];
 set password [lindex $argv 2];
 set network [lindex $argv 3];
+set rest [lrange $argv 4 end];
 
-spawn npm run deploy $contract $account -- --network $network
+eval spawn npm run deploy $contract $account $rest -- --network $network
 expect "Enter password for \"$account\":"
 send "$password\n"
 expect "$ "
