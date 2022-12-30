1bcc33b269e7e3e875857974d9f2f39f58083847
sebastiendan <sebastien.dan@gmail.com>
2022-12-30 14:54:47 -0500

    feat: adding gasLimit arg to deploy2.js

--- brownie/scripts/deploy2.js
+++ brownie/scripts/deploy2.js
@@ -1,21 +1,30 @@
 const axelarUtils = require('@axelar-network/axelar-gmp-sdk-solidity');
+const checksum = require('checksum');
 const ethers = require('ethers');
 const fs = require('fs');
 
 const CONST_ADDRESS_DEPLOYER_ADDR =
   '0x0000000000000000000000000000000000001110';
 
-const main = async function (endpoint, contractJsonPath, salt = '', args = []) {
+const main = async function (
+  endpoint,
+  contractJsonPath,
+  salt,
+  gasLimit = 0,
+  ...args
+) {
   const provider = new ethers.providers.JsonRpcProvider(endpoint);
-
-  provider
-    .getCode(CONST_ADDRESS_DEPLOYER_ADDR)
-    .then(console.log)
-    .catch(console.error);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
   let rawdata = fs.readFileSync(contractJsonPath);
   let contractJson = JSON.parse(rawdata);
+
+  console.log('ConstAddressDeployer address: ', CONST_ADDRESS_DEPLOYER_ADDR);
+  console.log('Wallet: ', wallet.address);
+  console.log('contractJson: ', checksum(JSON.stringify(contractJson)));
+  console.log('salt: ', salt);
+  console.log('args: ', args);
+  console.log('gasLimit: ', gasLimit);
 
   axelarUtils
     .deployContractConstant(
@@ -24,8 +33,15 @@ const main = async function (endpoint, contractJsonPath, salt = '', args = []) {
       contractJson,
       salt,
       args,
-      (gasLimit = null),
+      gasLimit === 0 ? null : gasLimit,
     )
+    .then((contract) => {
+      console.info(
+        `Successfully deployed ${contractJsonPath.split('.json')[0]} at ${
+          contract.address
+        }`,
+      );
+    })
     .catch(console.error);
 };
 
