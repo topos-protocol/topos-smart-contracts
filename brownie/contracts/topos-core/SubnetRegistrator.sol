eb21d77d4c15f6e582c5cdab115c22b2f04c13b0
Jawad Tariq <sjcool420@hotmail.co.uk>
2023-01-12 18:58:21 -0500

    feat: add CRUD storage for registering subnets
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

--- brownie/contracts/topos-core/SubnetRegistrator.sol
+++ brownie/contracts/topos-core/SubnetRegistrator.sol
@@ -1,29 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-type SubnetPublicKey is bytes32;
+import "./Bytes32Sets.sol";
+type SubnetId is bytes32;
 
 contract SubnetRegistrator {
-    error SubnetAlreadyRegistered(SubnetPublicKey publicKey);
-    error SubnetNotRegistered(SubnetPublicKey publicKey);
+    using Bytes32SetsLib for Bytes32SetsLib.Set;
 
     struct Subnet {
         string endpoint;
         string logoURL;
         string name;
         string currencySymbol;
-        bool isPresent;
     }
 
+    /// @notice Set of subnet public keys
+    Bytes32SetsLib.Set subnetSet;
+
     /// @notice Mapping to store the registered subnets
-    /// @dev SubnetPublicKey => Subnet
-    mapping(SubnetPublicKey => Subnet) public subnets;
+    /// @dev SubnetId => Subnet
+    mapping(SubnetId => Subnet) public subnets;
 
     /// @notice New subnet registration event
-    event NewSubnetRegistered(SubnetPublicKey publicKey);
+    event NewSubnetRegistered(SubnetId publicKey);
 
     /// @notice Subnet removal event
-    event SubnetRemoved(SubnetPublicKey publicKey);
+    event SubnetRemoved(SubnetId publicKey);
+
+    /// @notice Check if the subnet is already registered
+    /// @param publicKey FROST public key of a subnet
+    function subnetExists(SubnetId publicKey) external view returns (bool) {
+        return subnetSet.exists(SubnetId.unwrap(publicKey));
+    }
+
+    /// @notice Gets the count of the registered subnets
+    function getSubnetCount() external view returns (uint256) {
+        return subnetSet.count();
+    }
 
     /// @notice Register a new subnet
     /// @param endpoint JSON RPC endpoint of a subnet
@@ -35,19 +48,22 @@ contract SubnetRegistrator {
         string calldata endpoint,
         string calldata logoURL,
         string calldata name,
-        SubnetPublicKey publicKey,
+        SubnetId publicKey,
         string calldata currencySymbol
     ) public {
-        if (subnets[publicKey].isPresent) revert SubnetAlreadyRegistered(publicKey);
-        Subnet memory subnet = Subnet(endpoint, logoURL, name, currencySymbol, true);
-        subnets[publicKey] = subnet;
+        subnetSet.insert(SubnetId.unwrap(publicKey));
+        Subnet storage subnet = subnets[publicKey];
+        subnet.endpoint = endpoint;
+        subnet.logoURL = logoURL;
+        subnet.name = name;
+        subnet.currencySymbol = currencySymbol;
         emit NewSubnetRegistered(publicKey);
     }
 
     /// @notice Remove an already registered subnet
     /// @param publicKey FROST public key of a subnet
-    function removeSubnet(SubnetPublicKey publicKey) public {
-        if (!subnets[publicKey].isPresent) revert SubnetNotRegistered(publicKey);
+    function removeSubnet(SubnetId publicKey) public {
+        subnetSet.remove(SubnetId.unwrap(publicKey));
         delete subnets[publicKey];
         emit SubnetRemoved(publicKey);
     }
--- brownie/tests/unit/test_subnet_registrator.py
+++ brownie/tests/unit/test_subnet_registrator.py
@@ -14,6 +14,10 @@ def test_register_subnet_reverts_on_already_registered(
 
 def test_register_subnet_emits_event(alice, subnet_registrator):
     tx = register_subnet(alice, subnet_registrator)
+    assert (
+        subnet_registrator.subnetExists(c.SUBNET_PUBLIC_KEY, {"from": alice})
+        is True
+    )
     assert tx.events["NewSubnetRegistered"].values() == [
         brownie.convert.datatypes.HexString(c.SUBNET_PUBLIC_KEY, "bytes")
     ]
@@ -28,11 +32,19 @@ def test_remove_subnet_reverts_on_not_registered(alice, subnet_registrator):
 def test_remove_subnet_emits_event(alice, subnet_registrator):
     register_subnet(alice, subnet_registrator)
     tx = subnet_registrator.removeSubnet(c.SUBNET_PUBLIC_KEY, {"from": alice})
-    subnet = subnet_registrator.subnets(c.SUBNET_PUBLIC_KEY)
-    assert subnet["isPresent"] is False
+    assert (
+        subnet_registrator.subnetExists(c.SUBNET_PUBLIC_KEY, {"from": alice})
+        is False
+    )
     assert tx.events["SubnetRemoved"].values() == [
         brownie.convert.datatypes.HexString(c.SUBNET_PUBLIC_KEY, "bytes")
     ]
+
+
+def test_get_subnet_count_returns_count(alice, subnet_registrator):
+    subnet_count = 1
+    register_subnet(alice, subnet_registrator)
+    assert subnet_registrator.getSubnetCount({"from": alice}) == subnet_count
 
 
 # internal functions #
