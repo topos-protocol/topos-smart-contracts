1d6b33df89fd0ea2156aa63dc0e91806b3573e2a
Jawad Tariq <sjcool420@hotmail.co.uk>
2023-02-07 14:47:26 -0500

    feat: tx hex as input for asset transfer
    Signed-off-by: Jawad Tariq <sjcool420@hotmail.co.uk>

--- brownie/interfaces/IToposCore.sol
+++ brownie/interfaces/IToposCore.sol
@@ -99,7 +99,8 @@ interface IToposCore {
 
     function executeAssetTransfer(
         CertificateId certId,
-        bytes calldata crossSubnetTx,
+        uint256 index,
+        bytes calldata txn,
         bytes calldata crossSubnetTxProof
     ) external;
 
