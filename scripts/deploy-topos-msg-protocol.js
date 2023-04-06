65654eddf60fb03336e6bfa07d2a53596b08238f
Sébastien Dan <sebastien.dan@gmail.com>
2023-04-06 18:01:02 -0400

    fix: make lint pass

--- scripts/deploy-topos-msg-protocol.js
+++ scripts/deploy-topos-msg-protocol.js
@@ -1,212 +1,143 @@
-const axelarUtils = require('@axelar-network/axelar-gmp-sdk-solidity');
-const ethers = require('ethers');
+const axelarUtils = require('@axelar-network/axelar-gmp-sdk-solidity')
+const ethers = require('ethers')
 
-const tokenDeployerJSON = require('../build/contracts/TokenDeployer.json');
-const toposCoreJSON = require('../build/contracts/ToposCore.json');
-const toposCoreProxyJSON = require('../build/contracts/ToposCoreProxy.json');
-const toposCoreInterfaceJSON = require('../build/contracts/IToposCore.json');
+const tokenDeployerJSON = require('../build/contracts/TokenDeployer.json')
+const toposCoreJSON = require('../build/contracts/ToposCore.json')
+const toposCoreProxyJSON = require('../build/contracts/ToposCoreProxy.json')
+const toposCoreInterfaceJSON = require('../build/contracts/IToposCore.json')
 
-const CONST_ADDRESS_DEPLOYER_ADDR =
-  '0x0000000000000000000000000000000000001110';
+const CONST_ADDRESS_DEPLOYER_ADDR = '0x0000000000000000000000000000000000001110'
 
 const main = async function (endpoint, _sequencerPrivateKey) {
-  const provider = new ethers.providers.JsonRpcProvider(endpoint);
-  const toposDeployerPrivateKey = sanitizeHexString(process.env.PRIVATE_KEY);
-  const tokenDeployerSalt = process.env.TOKEN_DEPLOYER_SALT;
-  const toposCoreSalt = process.env.TOPOS_CORE_SALT;
-  const toposCoreProxySalt = process.env.TOPOS_CORE_PROXY_SALT;
+  const provider = new ethers.providers.JsonRpcProvider(endpoint)
+  const toposDeployerPrivateKey = sanitizeHexString(process.env.PRIVATE_KEY)
+  const tokenDeployerSalt = process.env.TOKEN_DEPLOYER_SALT
+  const toposCoreSalt = process.env.TOPOS_CORE_SALT
+  const toposCoreProxySalt = process.env.TOPOS_CORE_PROXY_SALT
 
   if (!_sequencerPrivateKey) {
-    console.error('ERROR: Please provide the sequencer private key!');
-    process.exit(1);
+    console.error('ERROR: Please provide the sequencer private key!')
+    process.exit(1)
   }
 
-  const sequencerPrivateKey = sanitizeHexString(_sequencerPrivateKey);
+  const sequencerPrivateKey = sanitizeHexString(_sequencerPrivateKey)
 
   if (!ethers.utils.isHexString(sequencerPrivateKey, 32)) {
-    console.error('ERROR: The sequencer private key is not a valid key!');
-    process.exit(1);
+    console.error('ERROR: The sequencer private key is not a valid key!')
+    process.exit(1)
   }
 
-  const isCompressed = true;
+  const isCompressed = true
   const sequencerPublicKey = ethers.utils.computePublicKey(
     sequencerPrivateKey,
-    isCompressed,
-  );
+    isCompressed
+  )
 
-  const subnetId = sanitizeHexString(sequencerPublicKey.substring(4));
-  console.log('Subnet Id:', subnetId);
+  const subnetId = sanitizeHexString(sequencerPublicKey.substring(4))
+  console.log('Subnet Id:', subnetId)
 
   if (
     !toposDeployerPrivateKey ||
     !ethers.utils.isHexString(toposDeployerPrivateKey, 32)
   ) {
     console.error(
-      'ERROR: Please provide a valid toposDeployer private key! (PRIVATE_KEY)',
-    );
-    process.exit(1);
+      'ERROR: Please provide a valid toposDeployer private key! (PRIVATE_KEY)'
+    )
+    process.exit(1)
   }
 
   if (!tokenDeployerSalt) {
     console.error(
-      'ERROR: Please provide a salt for TokenDeployer! (TOKEN_DEPLOYER_SALT)',
-    );
-    process.exit(1);
+      'ERROR: Please provide a salt for TokenDeployer! (TOKEN_DEPLOYER_SALT)'
+    )
+    process.exit(1)
   }
 
   if (!toposCoreSalt) {
     console.error(
-      'ERROR: Please provide a salt for ToposCore! (TOPOS_CORE_SALT)',
-    );
-    return;
+      'ERROR: Please provide a salt for ToposCore! (TOPOS_CORE_SALT)'
+    )
+    return
   }
 
   if (!toposCoreProxySalt) {
     console.error(
-      'ERROR: Please provide a salt for ToposCoreProxy! (TOPOS_CORE_PROXY_SALT)',
-    );
-    process.exit(1);
+      'ERROR: Please provide a salt for ToposCoreProxy! (TOPOS_CORE_PROXY_SALT)'
+    )
+    process.exit(1)
   }
 
-  const wallet = new ethers.Wallet(toposDeployerPrivateKey, provider);
+  const wallet = new ethers.Wallet(toposDeployerPrivateKey, provider)
 
-  console.info(`\nVerifying if TokenDeployer is already deployed...`);
-
-  const existingTokenDeployerAddress = await predictContractAddress(
+  const tokenDeployerAddress = await processContract(
+    'TokenDeployer',
     wallet,
     tokenDeployerJSON,
-    tokenDeployerSalt,
-  );
+    tokenDeployerSalt
+  )
 
-  const tokenDeployerCode = await provider.getCode(
-    existingTokenDeployerAddress,
-  );
-
-  if (tokenDeployerCode !== '0x') {
-    console.info(
-      `TokenDeployer is already deployed! (${existingTokenDeployerAddress})`,
-    );
-  } else {
-    console.info(`Deploying TokenDeployer with constant address...`);
-
-    const tokenDeployerAddress = await deployConstAddress(
-      wallet,
-      tokenDeployerJSON,
-      tokenDeployerSalt,
-    );
-
-    console.info(
-      `Successfully deployed TokenDeployer at ${tokenDeployerAddress}\n`,
-    );
-  }
-
-  console.info(`\nVerifying if ToposCore is already deployed...`);
-
-  const existingToposCoreAddress = await predictContractAddress(
+  const toposCoreAddress = await processContract(
+    'ToposCore',
     wallet,
     toposCoreJSON,
     toposCoreSalt,
-    [existingTokenDeployerAddress || tokenDeployerAddress],
-  );
+    [tokenDeployerAddress],
+    4_000_000
+  )
 
-  const toposCoreCode = await provider.getCode(existingToposCoreAddress);
-
-  if (toposCoreCode !== '0x') {
-    console.info(
-      `ToposCore is already deployed! (${existingToposCoreAddress})`,
-    );
-  } else {
-    console.info(`Deploying ToposCore...`);
-
-    const toposCoreAddress = await deployConstAddress(
-      wallet,
-      toposCoreJSON,
-      toposCoreSalt,
-      [existingTokenDeployerAddress || tokenDeployerAddress],
-      4_000_000,
-    );
-
-    console.info(`Successfully deployed ToposCore at ${toposCoreAddress}\n`);
-  }
-
-  console.info(`\nVerifying if ToposCoreProxy is already deployed...`);
-
-  const params = ethers.utils.defaultAbiCoder.encode(
+  const toposCoreProxyParams = ethers.utils.defaultAbiCoder.encode(
     ['address[]', 'uint256'],
-    [[wallet.address], 1], // TODO: Use a different admin address than ToposDeployer
-  );
-
-  const existingToposCoreProxyAddress = await predictContractAddress(
+    [[wallet.address], 1] // TODO: Use a different admin address than ToposDeployer
+  )
+  const toposCoreProxyAddress = await processContract(
+    'ToposCoreProxy',
     wallet,
     toposCoreProxyJSON,
     toposCoreProxySalt,
-    [existingToposCoreAddress || toposCoreAddress, params],
-  );
+    [toposCoreAddress, toposCoreProxyParams],
+    4_000_000
+  )
 
-  const toposCoreProxyCode = await provider.getCode(
-    existingToposCoreProxyAddress,
-  );
-
-  if (toposCoreProxyCode !== '0x') {
-    console.info(
-      `ToposCoreProxy is already deployed! (${existingToposCoreProxyAddress})`,
-    );
-  } else {
-    console.info(`Deploying ToposCoreProxy with constant address...`);
-
-    const toposCoreProxyAddress = await deployConstAddress(
-      wallet,
-      toposCoreProxyJSON,
-      toposCoreProxySalt,
-      [existingToposCoreAddress || toposCoreAddress, params],
-      4_000_000,
-    );
-
-    console.info(
-      `Successfully deployed ToposCoreProxy at ${toposCoreProxyAddress}\n`,
-    );
-  }
-
-  console.info(`\nSetting subnetId on ToposCore via proxy`);
+  console.info(`\nSetting subnetId on ToposCore via proxy`)
   const toposCoreInterface = new ethers.Contract(
-    existingToposCoreProxyAddress || toposCoreProxyAddress,
+    toposCoreProxyAddress,
     toposCoreInterfaceJSON.abi,
-    wallet,
-  );
+    wallet
+  )
   await toposCoreInterface
     .setNetworkSubnetId(subnetId, { gasLimit: 4_000_000 })
     .then(async (tx) => {
       await tx.wait().catch((error) => {
         console.error(
-          `Error: Failed (wait) to set ${subnetId} subnetId on ToposCore via proxy!`,
-        );
-        console.error(error);
-        process.exit(1);
-      });
+          `Error: Failed (wait) to set ${subnetId} subnetId on ToposCore via proxy!`
+        )
+        console.error(error)
+        process.exit(1)
+      })
     })
     .catch((error) => {
       console.error(
-        `Error: Failed to set ${subnetId} subnetId on ToposCore via proxy!`,
-      );
-      console.error(error);
-      process.exit(1);
-    });
-  const networkSubnetId = await toposCoreInterface.networkSubnetId();
+        `Error: Failed to set ${subnetId} subnetId on ToposCore via proxy!`
+      )
+      console.error(error)
+      process.exit(1)
+    })
+  const networkSubnetId = await toposCoreInterface.networkSubnetId()
   console.info(
-    `Successfully set ${networkSubnetId} subnetId on ToposCore via proxy\n`,
-  );
-};
+    `Successfully set ${networkSubnetId} subnetId on ToposCore via proxy\n`
+  )
+}
 
 const sanitizeHexString = function (hexString) {
-  return hexString.startsWith('0x') ? hexString : `0x${hexString}`;
-};
+  return hexString.startsWith('0x') ? hexString : `0x${hexString}`
+}
 
 const deployConstAddress = function (
   wallet,
   contractJson,
   salt,
   args,
-  gasLimit = 0,
+  gasLimit
 ) {
   return axelarUtils
     .deployContractConstant(
@@ -215,14 +146,14 @@ const deployConstAddress = function (
       contractJson,
       salt,
       args,
-      gasLimit === 0 ? null : gasLimit,
+      gasLimit === 0 ? null : gasLimit
     )
     .then((contract) => contract.address)
     .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-};
+      console.error(error)
+      process.exit(1)
+    })
+}
 
 const predictContractAddress = function (wallet, contractJson, salt, args) {
   return axelarUtils
@@ -231,13 +162,68 @@ const predictContractAddress = function (wallet, contractJson, salt, args) {
       wallet,
       contractJson,
       salt,
-      args,
+      args
     )
     .catch((error) => {
-      console.error(error);
-      process.exit(1);
-    });
-};
+      console.error(error)
+      process.exit(1)
+    })
+}
 
-const args = process.argv.slice(2);
-main(...args);
+const processContract = async function (
+  contractName,
+  wallet,
+  contractJson,
+  salt,
+  args = [],
+  gasLimit = 0
+) {
+  console.info(`\nVerifying if ${contractName} is already deployed...`)
+
+  const predictedContractAddress = await predictContractAddress(
+    wallet,
+    contractJson,
+    salt,
+    args
+  )
+
+  const codeAtPredictedAddress = await wallet.provider.getCode(
+    predictContractAddress
+  )
+
+  const thereIsCodeAtAddress = codeAtPredictedAddress !== '0x'
+
+  if (thereIsCodeAtAddress) {
+    console.info(
+      `${contractName} is already deployed! (${predictedContractAddress})`
+    )
+
+    return predictedContractAddress
+  } else {
+    console.info(`Deploying ${contractName} with constant address...`)
+
+    const newContractAddress = await deployConstAddress(
+      wallet,
+      contractJson,
+      salt,
+      args,
+      gasLimit
+    )
+
+    if (newContractAddress !== predictedContractAddress) {
+      console.error(
+        `ERROR: New contract address doesn't match the predicted address! (New: ${newContractAddress} | Predicted: ${predictedContractAddress})`
+      )
+      process.exit(1)
+    }
+
+    console.info(
+      `Successfully deployed ${contractName} at ${newContractAddress}\n`
+    )
+
+    return newContractAddress
+  }
+}
+
+const args = process.argv.slice(2)
+main(...args)
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,8 @@
-const axelarUtils = require('@axelar-network/axelar-gmp-sdk-solidity');
-const ethers = require('ethers');
-const fs = require('fs');
+const axelarUtils = require('@axelar-network/axelar-gmp-sdk-solidity')
+const ethers = require('ethers')
+const fs = require('fs')
 
-const CONST_ADDRESS_DEPLOYER_ADDR =
-  '0x0000000000000000000000000000000000001110';
+const CONST_ADDRESS_DEPLOYER_ADDR = '0x0000000000000000000000000000000000001110'
 
 const main = async function (
   endpoint,
@@ -12,34 +11,34 @@ const main = async function (
   gasLimit = 0,
   ...args
 ) {
-  const provider = new ethers.providers.JsonRpcProvider(endpoint);
-  const privateKey = process.env.PRIVATE_KEY;
+  const provider = new ethers.providers.JsonRpcProvider(endpoint)
+  const privateKey = process.env.PRIVATE_KEY
 
   if (!privateKey || !ethers.utils.isHexString(privateKey, 32)) {
-    console.error('ERROR: Please provide a valid private key! (PRIVATE_KEY)');
-    return;
+    console.error('ERROR: Please provide a valid private key! (PRIVATE_KEY)')
+    return
   }
 
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 
-  let rawdata;
+  let rawdata
   try {
-    rawdata = fs.readFileSync(contractJsonPath);
+    rawdata = fs.readFileSync(contractJsonPath)
   } catch (error) {
     console.error(
-      `ERROR: Could not find a contract JSON file at ${contractJsonPath}`,
-    );
-    return;
+      `ERROR: Could not find a contract JSON file at ${contractJsonPath}`
+    )
+    return
   }
 
-  let contractJson;
+  let contractJson
   try {
-    contractJson = JSON.parse(rawdata);
+    contractJson = JSON.parse(rawdata)
   } catch (error) {
     console.error(
-      `ERROR: Could not parse the contract JSON file found at ${contractJsonPath}`,
-    );
-    return;
+      `ERROR: Could not parse the contract JSON file found at ${contractJsonPath}`
+    )
+    return
   }
 
   axelarUtils
@@ -49,17 +48,17 @@ const main = async function (
       contractJson,
       salt,
       args,
-      gasLimit === 0 ? null : gasLimit,
+      gasLimit === 0 ? null : gasLimit
     )
     .then((contract) => {
       console.info(
         `Successfully deployed ${contractJsonPath.split('.json')[0]} at ${
           contract.address
-        }`,
-      );
+        }`
+      )
     })
-    .catch(console.error);
-};
+    .catch(console.error)
+}
 
-const args = process.argv.slice(2);
-main(...args);
+const args = process.argv.slice(2)
+main(...args)
